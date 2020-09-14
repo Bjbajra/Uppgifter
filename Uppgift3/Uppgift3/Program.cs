154c17e13c498c3cda48568f27e909bc36e64e2e
DESKTOP-9H83LJ6\bijay <bijay.bajrcharya84@gmail.com>
2020-09-14 18:25:44 +0200

    added uppgift 1 and 3

---
+++ Uppgift3/Uppgift3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Uppgift3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Skriv ett program som frågar användaren efter ett tal. Det ska skriva ut om talet är mindre än 100, lika med 100 eller större.*/
+
+            Console.Write("Skriv in ett tal: ");
+            int tal = int.Parse(Console.ReadLine());
+
+            if (tal < 100)
+            {
+                Console.WriteLine($"{tal} är mindre än 100.");
+            }
+            else if (tal == 100)
+            {
+                Console.WriteLine($"{tal} är lika med 100.");
+            }
+            else
+                Console.WriteLine($"{tal} är större än 100.");
+        }
+    }
+}
