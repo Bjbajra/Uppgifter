0283b2eca567d147d30378d9dd28d71fc915e192
DESKTOP-9H83LJ6\bijay <bijay.bajrcharya84@gmail.com>
2020-09-14 13:41:08 +0200

    Upload first Lab

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in en menining: ");
+            string inputStr = Console.ReadLine();
+            Console.WriteLine();
+
+            char checkValue;
+            long sum = 0;
+
+            for (int i = 0; i < inputStr.Length; i++)
+            {
+                checkValue = inputStr[i];
+
+                for (int j = i + 1; j < inputStr.Length; j++)
+                {
+                    if (!Char.IsNumber(inputStr, j)) break;
+
+                    if (inputStr[j] == checkValue)
+                    {
+                        string preStr = inputStr.Substring(0, i);
+                        string midStr = inputStr.Substring(i, j - i + 1);
+                        string endStr = inputStr.Substring(j + 1, inputStr.Length - j - 1);
+
+                        Console.Write(preStr);
+
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        Console.Write(midStr);
+                        Console.ResetColor();
+
+                        Console.WriteLine(endStr);
+
+                        sum += long.Parse(midStr);
+                        
+                        break;
+                    }
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"\nSumman av alla delsträng är : {sum}");
+            Console.ResetColor();
+        }
+
+    }
+}
---
+++ Uppgift1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Uppgift1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Skriv ett program som frågar efter användarens namn och skriver ut en hälsning på konsolen.
+             * Om namnet är "David" ska det skriva ut "Hej David!"*/
+
+            /*Console.WriteLine("Vad heter du?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Hej {name} !");*/
+            
+            
+
+            /*Uppdatera programmet i uppgift 1 så att det även frågar efter ett tal. Skriv hälsningen så många gånger som användaren anger.*/
+
+            Console.WriteLine("Vad heter du?");
+            string name = Console.ReadLine();
+            Console.Write("Skriv ett tal: ");
+            int num = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < num; i++)
+            { 
+               
+                    Console.WriteLine($"Hej {name}!");
+              
+            }
+           
+            
+
+        }
+    }
+}
