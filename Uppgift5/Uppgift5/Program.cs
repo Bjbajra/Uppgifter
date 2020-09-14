3e2c39888d5f30cf75ef683ca86ee83ccd6d886b
DESKTOP-9H83LJ6\bijay <bijay.bajrcharya84@gmail.com>
2020-09-14 18:33:23 +0200

    sum and average uppgit5

---
+++ Uppgift5/Uppgift5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Uppgift5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Skriv ett program som upprepade gånger frågar användaren efter ett tal
+             * ända till man skriver något som inte är ett tal (t.ex bara trycker enter).
+             * Efter varje inmatning ska summan av alla tal som matats in skrivas ut, innan nästa inmatning efterfrågas. 
+             * Innan programmet avslutas ska man även skriva ut medelvärde av de inmatade talen. Hint: TryParse() */
+
+            bool isRunning; 
+            double sum = 0;
+            int countNum = 0;
+
+
+            do
+            {
+                Console.Write("Skriv ett tal: ");
+                isRunning = Int32.TryParse(Console.ReadLine(), out int number);
+                sum += number;
+                Console.WriteLine($"Summan av alla tal är: {sum}");
+                if (isRunning) countNum++;
+
+            } while (isRunning == true);
+
+            Console.WriteLine($"Medelvärde av de inmatade talen är: {sum / countNum}");
+            
+        }
+    }
+}
