60a4f74ad2fe357234c1808e018d0b64ed7eb7b9
DESKTOP-9H83LJ6\bijay <bijay.bajrcharya84@gmail.com>
2020-10-19 09:02:34 +0200

    Resolution calculation fixed.

--- Lab3/Lab3/Program.cs
+++ Lab3/Lab3/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Drawing;
-
 
 namespace Lab3
 {
@@ -10,9 +8,17 @@ namespace Lab3
     {
         static void Main(string[] args)
         {
-            string filePath = Path.GetFullPath(@"C:\Users\bijay\OneDrive\Bilder\Test_photos\test1.png");
-            string fileExtension = Path.GetExtension(filePath);
-
+            //string filePath = Path.GetFullPath(@"C:\Users\bijay\OneDrive\Bilder\Test_photos\pic.bmp");
+            string filePath;
+            if (args.Length != 0)
+            {
+                filePath = args[0];
+            }
+            else
+            {
+                Console.Write("Give the path for file: ");
+                filePath = Console.ReadLine();
+            }
             if (!File.Exists(filePath))
             {
                 Console.WriteLine("File not found.");
@@ -21,17 +27,16 @@ namespace Lab3
             {
                 if (filePath.IsImage(out IsImageExtension.ImageType type))
                 {
-                    string ext = type.ToString().ToLower();
-                    var img = Image.FromFile(filePath, true);
-                    Console.WriteLine($"This is a {ext} image. Resolution: {img.Width}x{img.Height} pixels.");
-
                     FileStream fsSource = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+                    string ext = type.ToString().ToLower();
+                    var img = IsImageExtension.GetResolution(fsSource, type);
+                    Console.WriteLine($"This is a {ext} image. Resolution: {img} pixels.");
+
                     byte[] bytes = new byte[fsSource.Length];
                     int numBytesToRead = (int)fsSource.Length;
                     int numBytesRead = 0;
                     while (numBytesToRead > 0)
                     {
-                        
                         int n = fsSource.Read(bytes, numBytesRead, numBytesToRead);
                         // Break when the end of the file is reached.
                         if (n == 0)
@@ -40,29 +45,52 @@ namespace Lab3
                         numBytesRead += n;
                         numBytesToRead -= n;
                     }
-
                     Console.WriteLine($"File Size in Bytes:  {bytes.Length}");
                 }
                 else
                 {
-                    Console.WriteLine($"This is a not a valid {fileExtension} file!");
+                    Console.WriteLine($"This is a not a valid .bmp or .png file!");
                 }
             }
-
         }
     }
-
     public static class IsImageExtension
     {
-       
         static List<string> bmp;
         static List<string> png;
-
         public enum ImageType
         {
             BMP,
             PNG,
             NONE
+        }
+        public static string GetResolution(FileStream fileStream, ImageType imgType)
+        {
+            var data = new byte[8];
+            var width = 0;
+            var height = 0;
+
+            switch (imgType)
+            {
+                case ImageType.BMP:
+                    fileStream.Seek(18, SeekOrigin.Begin);
+                    fileStream.Read(data, 0, 8);
+
+                    width = data[0] + (data[1] << 8) + (data[2] << 16) + (data[3] << 24);
+                    height = data[4] + (data[5] << 8) + (data[6] << 16) + (data[7] << 24);
+
+                    return $"{width}x{height}";
+
+                case ImageType.PNG:
+                    fileStream.Seek(16, SeekOrigin.Begin);
+                    fileStream.Read(data, 0, 8);
+
+                    width = data[3] + (data[2] << 8) + (data[1] << 16) + (data[0] << 24);
+                    height = data[7] + (data[6] << 8) + (data[5] << 16) + (data[4] << 24);
+
+                    return $"{width} x {height}";
+            }
+            return null;
         }
 
         const string BMP = "42";
@@ -82,6 +110,7 @@ namespace Lab3
             using (var stream = File.OpenRead(file))
                 return stream.IsImage(out type);
         }
+
         public static bool IsImage(this Stream stream, out ImageType type)
         {
             type = ImageType.NONE;
@@ -103,8 +132,6 @@ namespace Lab3
                         type = ImageType.PNG;
                         return true;
                     }
-                    break;
-                default:
                     break;
             }
             return false;
