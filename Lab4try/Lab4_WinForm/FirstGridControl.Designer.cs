cc8911a8a6c3a4ed44b7346e49ea3995e2ab432d
DESKTOP-9H83LJ6\bijay <bijay.bajrcharya84@gmail.com>
2020-10-26 09:46:50 +0100

    trying translate program

---
+++ Lab4try/Lab4_WinForm/FirstGridControl.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Lab4_WinForm
+{
+    partial class FirstGridControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle10 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle11 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle12 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.English = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Swedish = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Spanish = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nepali = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Addbutton = new System.Windows.Forms.Button();
+            this.Removebutton = new System.Windows.Forms.Button();
+            this.Practicebutton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.English,
+            this.Swedish,
+            this.Spanish,
+            this.Nepali});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 25);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(749, 534);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // English
+            // 
+            dataGridViewCellStyle9.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.English.DefaultCellStyle = dataGridViewCellStyle9;
+            this.English.HeaderText = "ENGLISH";
+            this.English.Name = "English";
+            this.English.ReadOnly = true;
+            // 
+            // Swedish
+            // 
+            dataGridViewCellStyle10.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle10.ForeColor = System.Drawing.Color.Green;
+            this.Swedish.DefaultCellStyle = dataGridViewCellStyle10;
+            this.Swedish.HeaderText = "SWEDISH";
+            this.Swedish.Name = "Swedish";
+            this.Swedish.ReadOnly = true;
+            // 
+            // Spanish
+            // 
+            dataGridViewCellStyle11.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle11.ForeColor = System.Drawing.Color.Red;
+            this.Spanish.DefaultCellStyle = dataGridViewCellStyle11;
+            this.Spanish.HeaderText = "SPANISH";
+            this.Spanish.Name = "Spanish";
+            this.Spanish.ReadOnly = true;
+            // 
+            // Nepali
+            // 
+            dataGridViewCellStyle12.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle12.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.Nepali.DefaultCellStyle = dataGridViewCellStyle12;
+            this.Nepali.HeaderText = "NEPALI";
+            this.Nepali.Name = "Nepali";
+            this.Nepali.ReadOnly = true;
+            // 
+            // Addbutton
+            // 
+            this.Addbutton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Addbutton.Location = new System.Drawing.Point(493, 565);
+            this.Addbutton.Name = "Addbutton";
+            this.Addbutton.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Addbutton.Size = new System.Drawing.Size(75, 23);
+            this.Addbutton.TabIndex = 1;
+            this.Addbutton.Text = "Add word";
+            this.Addbutton.UseVisualStyleBackColor = true;
+            // 
+            // Removebutton
+            // 
+            this.Removebutton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Removebutton.Location = new System.Drawing.Point(574, 565);
+            this.Removebutton.Name = "Removebutton";
+            this.Removebutton.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Removebutton.Size = new System.Drawing.Size(91, 23);
+            this.Removebutton.TabIndex = 2;
+            this.Removebutton.Text = "Remove word";
+            this.Removebutton.UseVisualStyleBackColor = true;
+            // 
+            // Practicebutton
+            // 
+            this.Practicebutton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Practicebutton.Location = new System.Drawing.Point(671, 565);
+            this.Practicebutton.Name = "Practicebutton";
+            this.Practicebutton.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Practicebutton.Size = new System.Drawing.Size(75, 23);
+            this.Practicebutton.TabIndex = 3;
+            this.Practicebutton.Text = "Practice";
+            this.Practicebutton.UseVisualStyleBackColor = true;
+            // 
+            // FirstGridControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.Practicebutton);
+            this.Controls.Add(this.Removebutton);
+            this.Controls.Add(this.Addbutton);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "FirstGridControl";
+            this.Size = new System.Drawing.Size(749, 591);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Addbutton;
+        private System.Windows.Forms.Button Removebutton;
+        private System.Windows.Forms.Button Practicebutton;
+        private System.Windows.Forms.DataGridViewTextBoxColumn English;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Swedish;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Spanish;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nepali;
+    }
+}
---
+++ Lab4try/Word_Library/WordList.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+namespace Word_Library
+{
+    public class WordList
+    {
+        public string Name { get; }
+        public string[] Languages { get; }
+
+        private static readonly char[] ChArray = { ';' };
+
+        private readonly List<Word> myWords = new List<Word>();
+        public WordList(string name, params string[] languages)
+        {
+            Name = name;
+            Languages = languages;
+        }
+
+        public static string[] GetList()
+        {
+            var files = Directory.GetFiles(Folder.SpecificFolder)
+                .Select(Path.GetFileNameWithoutExtension)
+                .ToArray();
+            return files;
+        }
+
+        public static WordList LoadList(string name)
+        {
+            if (!File.Exists(Folder.GetFilePath(name.ToLower())))
+            {
+                var file = Folder.GetFilePath(name);
+                var fStream = File.Create(file);
+                fStream.Close();
+            }
+
+            var fileReader = new StreamReader(Folder.GetFilePath(name));
+            var line1 = fileReader.ReadLine();
+            if (line1 == null) return null;
+
+            var languages = line1.Split(ChArray, StringSplitOptions.RemoveEmptyEntries);
+            var wordList = new WordList(name, languages);
+            var line = " ";
+            while ((line = fileReader.ReadLine()) != null)
+            {
+                wordList.Add(line.Split(ChArray, StringSplitOptions.RemoveEmptyEntries));
+            }
+            return wordList;
+        }
+
+        public void Save()
+        {
+            var file = Folder.GetFilePath(Name);
+            var fStream = File.Create(file);
+            fStream.Close();
+            LoadList(Name);
+
+            foreach (var lang in Languages)
+            {
+                File.AppendAllText(file, $"{lang}");
+            }
+
+            foreach (var word in myWords)
+            {
+                File.AppendAllText(file, $"\n");
+
+                for (int i = 0; i < Languages.Length; i++)
+                {
+                    File.AppendAllText(file, $"{word.Translations[i]};");
+                }
+            }
+        }
+
+        public void Add(params string[] translations)
+        {
+            try
+            {
+                if (translations.Length != Languages.Length)
+                {
+                    myWords.Add(new Word());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public bool Remove(int translations, string word)
+        {
+            var remove_Index = 0;
+            if (myWords.Any(i => i.Translations[translations] == word))
+            {
+                remove_Index = myWords.IndexOf(myWords.First(i => i.Translations[translations] == word));
+            }
+
+            myWords.RemoveAt(remove_Index);
+            Save();
+            return true;
+        }
+
+        public int Count()
+        {
+            return myWords.Count;
+        }
+
+        public void List(int sortByTranslations, Action<string[]> showTranslations)
+        {
+            var sortedTranslations = myWords.OrderBy(x => x.Translations[sortByTranslations]).ToArray();
+            LoadList(Name);
+            
+            foreach (var translation in sortedTranslations)
+            {
+                showTranslations(translation.Translations);
+            }
+        }
+
+        public Word GetWordToPractice()
+        {
+            Random rnd = new Random();
+            int randomWord = rnd.Next(myWords.Count);
+            int fromLanguage = rnd.Next(Languages.Length);
+            int toLanguage = rnd.Next(Languages.Length);
+
+            while (toLanguage == fromLanguage)
+            {
+                toLanguage = rnd.Next(Languages.Length);
+            }
+
+            Word word = new Word(rnd.Next(Languages.Length),
+                rnd.Next(Languages.Length), myWords[randomWord].Translations);
+            return word;
+
+        }
+    }
+
+    public class Folder
+    {
+        private static string PathFolder => Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+        public static string SpecificFolder => Path.Combine(PathFolder, "Lab4");
+
+        public static void CreateFolder()
+        {
+            Directory.CreateDirectory(SpecificFolder);
+        }
+
+        public static string GetFilePath(string name)
+        {
+            return $@"{SpecificFolder}\{name}.dat";
+        }
+    }
+}
